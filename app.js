f2a0f530cc9c1e52f58899b4ea77ec53c2192354
MathieuMolina <molina.mathieu@hotmail.com>
2022-03-23 09:32:44 +0100

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+let carre = document.getElementById("carre");
+let button = document.getElementById ("button");
+
+button.addEventListener("click", function(){
+    carre.classList.toggle("mystyle");
+});
